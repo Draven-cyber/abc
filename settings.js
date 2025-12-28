859fb7cc142e0f1d91a3347b47457fa8eafc9430
Draven-cyber <pixelpassportgaming@gmail.com>
2025-12-28 11:11:51 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,8 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Your SESSION ID Put' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=45f9cbbf859a1b32' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
